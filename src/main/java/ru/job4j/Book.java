ce6064cc5e5e10b640a291aa216b6a0dabccc78a
andrey <avbaranov@internet.ru>
2024-06-28 11:47:35 +1000

    adding book to main branch

---
+++ src/main/java/ru/job4j/Book.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Book {
+    private String title;
+    private double cost;
+
+    public Book(String title, double cost) {
+        this.title = title;
+        this.cost = cost;
+    }
+}
