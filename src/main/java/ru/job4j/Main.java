36c4bb643b161f170faac3c23fd9001668cbeee8
andrey <avbaranov@internet.ru>
2024-06-28 11:38:02 +1000

    initial commit for branch fix

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -3,5 +3,8 @@ package ru.job4j;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello from branch fix");
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
     }
 }
