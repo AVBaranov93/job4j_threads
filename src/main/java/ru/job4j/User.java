35d044af780fa3bda951e74f629cd56ce8657f3a
andrey <avbaranov@internet.ru>
2024-06-28 11:43:25 +1000

    added User to fix branch

---
+++ src/main/java/ru/job4j/User.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+}
