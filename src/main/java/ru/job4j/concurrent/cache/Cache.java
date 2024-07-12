8d0eb4e4d09db20fe91fdd6961607850195374ff
AVBaranov93 <avbaranov@internet.ru>
2024-07-12 17:38:00 +1000

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/concurrent/cache/Cache.java
@@ -0,0 +1,33 @@
+package ru.job4j.concurrent.cache;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.id(), model) == null;
+    }
+
+    public boolean update(Base model) throws OptimisticException {
+        var stored = findById(model.id()).orElse(null);
+        if (stored != null && stored.version() != model.version()) {
+            throw new OptimisticException("version is not valid");
+        }
+        return memory.computeIfPresent(model.id(), (k, v) -> new Base(k, model.name(), model.version() + 1)) != null;
+    }
+
+    public void delete(int id) {
+        memory.entrySet().removeIf(e -> e.getValue().version() == id);
+    }
+
+    public Optional<Base> findById(int id) {
+        return Stream.of(memory.get(id))
+                .filter(Objects::nonNull)
+                .findFirst();
+    }
+}
