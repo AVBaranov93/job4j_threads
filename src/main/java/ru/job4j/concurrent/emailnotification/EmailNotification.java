a90c68dc67915ead267bc28ccdcccab522074a9f
AVBaranov93 <avbaranov@internet.ru>
2024-07-31 14:23:03 +1000

    2. ExecutorService рассылка почты. [#63097 #507273]

--- src/main/java/ru/job4j/concurrent/emailnotification/EmailNotification.java
+++ src/main/java/ru/job4j/concurrent/emailnotification/EmailNotification.java
@@ -5,6 +5,7 @@ import java.util.concurrent.Executors;
 
 public class EmailNotification {
     private final ExecutorService pool = Executors.newSingleThreadExecutor();
+
     public synchronized void emailTo(User user) {
         String subject = String.format("Notification %s to email %s", user.getUsername(), user.getEmail());
         String body = String.format("Add a new event to %s", user.getUsername());
@@ -15,6 +16,13 @@ public class EmailNotification {
 
     public synchronized void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public synchronized void send(String subject, String body, String email) {
